4a5e4f7f0a7866e6646be1e5d0a6c998b3bcabe1
Kartik Saxena <karrtiksaxena@gmail.com>
2024-02-22 02:05:30 +0530

    use effect on the rocks

--- components/UseEffectHook.jsx
+++ components/UseEffectHook.jsx
@@ -8,18 +8,27 @@ function UseEffectHook () {
   
 const [count, setCount] =  useState(0);
 
+const [showtext, isShowtext] = useState(false);
+
 
 useEffect(()=>{
-  console.log("hello paaji");
-
-},[])
+  if (count===5){
+    isShowtext(true);
+  }
+  else if (count ===7){
+    isShowtext(false);
+  }
+},[count])
 
 
   return (
     <div>
       <h1 className="text-2xl justify-center">Use effect</h1>
       <p className='text-xl justify-center'>Count is {count}</p>
-      <h3></h3>
+      {
+        showtext ? <h3>Hello bhai rukja</h3> : null
+      }
+      
       <button className="rounded-lg bg-slate-500 text-center p-4 bg-blend-color-burn"onClick={()=>setCount(count+1)}>Increase Count</button>
     </div>
   )
